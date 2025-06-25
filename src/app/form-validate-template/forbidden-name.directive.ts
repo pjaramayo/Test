1e40cf42842874d606be8eee695f2a25a54bed4c
Pablo Aramayo <pjaramayo@gmail.com>
2025-06-25 17:52:43 -0300

    Se completo teroria

--- src/app/form-validate-template/forbidden-name.directive.ts
+++ src/app/form-validate-template/forbidden-name.directive.ts
@@ -1,27 +1,32 @@
 import { Directive, Input } from '@angular/core';
 import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn, ValidationErrors } from '@angular/forms';
 
-export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const forbidden = nameRe.test(control.value);
-    return forbidden ? { forbiddenName: { value: control.value } } : null;
-  };
-}
 
 @Directive({
-  selector: '[appForbiddenName]',//<input type="text" appForbiddenName="Ramón" /> El selector en @directive es un atributo(clve Valor)
+  selector: '[appForbiddenName]',//<input type="text" appForbiddenName="Ramón" /> El selector en @directive es un atributo(clave Valor)
   standalone: true, // ¡ESTO ES CRUCIAL PARA UN COMPONENTE STANDALONE!
   providers: [{ provide: NG_VALIDATORS, useExisting: ForbiddenValidatorDirective, multi: true }]
 })
 
 export class ForbiddenValidatorDirective implements Validator {
   @Input('appForbiddenName') forbiddenName: string = '';
-
-  validate(control: AbstractControl): ValidationErrors | null {
+  //appForbiddenName es un alias que enlaza con el alias del html.
+  //forbiddenName es una propiedad para trabajar en este fichero.ts, esta contiene Ramón.
+  validate(control: AbstractControl): ValidationErrors | null { //Aqui se cheque que el input tenga un valor.
     if (!this.forbiddenName) {
       return null;
     }
     const forbiddenRegex = new RegExp(this.forbiddenName, 'i');
+    console.log('Esto es:'+ forbiddenRegex);
     return forbiddenNameValidator(forbiddenRegex)(control);
   }
+}
+
+export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+  console.log('iii',nameRe)
+  return (control: AbstractControl): ValidationErrors | null => {
+    const forbidden = nameRe.test(control.value);
+    console.log(forbidden)
+    return forbidden ? { forbiddenName: { value: control.value } } : null;
+  };
 }
