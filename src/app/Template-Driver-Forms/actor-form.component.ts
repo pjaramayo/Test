283fa43341884c8e7124feea3d21e107863b03d6
Pablo Aramayo <pjaramayo@gmail.com>
2025-08-06 17:54:24 -0300

    'TemplateDriver'

---
+++ src/app/Template-Driver-Forms/actor-form.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+import { Actor } from '../../Actor';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-actor-form',
+  standalone: true,
+  imports: [FormsModule, JsonPipe],
+  templateUrl: './actor-form.component.html',
+  styleUrl: './actor-form.component.css',
+})
+export class ActorFormComponent {
+  skills = ['Method Acting', 'Singing', 'Dancing', 'Swordfighting'];
+  model = new Actor(18, 'Tom Cruise', this.skills[3], 'CW Productions');
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+   newActor() {
+    this.model = new Actor(42, '', '');
+  }
+
+  showFormControls(form: any) {
+    return form && form.controls.name && form.controls.name.value;
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { ReactFormsComponent } from './Reactive Forms/form-control/react-forms.component';
 import { FormGrpComponent } from './Reactive Forms/form-group/form-grp.component';
@@ -5,6 +7,9 @@ import { FormBuilderComponent } from './Reactive Forms/form-builder/form-builder
 import { FormArrayComponent } from './Reactive Forms/form-array/form-array.component';
 import { FormValidateTemplateComponent } from './form-validate-template/form-validate-template.component';
 import { FormValidateReactiveComponent } from './form-validate-reactive/form-validate-reactive.component';
+import { ActorFormComponent } from './Template-Driver-Forms/actor-form.component';
+
+
 export const routes: Routes = [
   {
     path: '',
@@ -21,4 +26,5 @@ export const routes: Routes = [
   { path: 'form-array', component: FormArrayComponent },
   { path: 'form-validate-template', component: FormValidateTemplateComponent },
   { path: 'form-validate-reactive', component: FormValidateReactiveComponent },
+  { path: 'Template-Driver-Forms', component: ActorFormComponent },
 ];
